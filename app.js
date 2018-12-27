3850ee593891ddba682ea96e9eb1aa87dd0587ea
Rajeshwaran <rajeshfederer2@gmail.com>
2018-12-27 20:38:10 +0530

    content dispositon

--- app.js
+++ app.js
@@ -12,15 +12,26 @@ app.get('/policydocument', function(req, res){
     let fileName = req.query.fileName ? req.query.fileName.toLowerCase(): ""; 
     //let filePath = "./"+fileName+".pdf";
     let filePath = "./"+fileName;
-    console.log(filePath);
-    fs.readFile(filePath , function (err,data){
-        // res.setHeader("Content-Disposition","inline");
-        // res.contentType("application/pdf");
-        //res.setHeader('Content-Disposition', 'inline; filename=' + fileName + '.pdf');
-        res.setHeader('Content-Disposition', 'inline; filename=' + fileName );
-        res.setHeader('Content-Type', 'application/pdf');
-        res.send(data);
-    });
+    // console.log(filePath);
+    // fs.readFile(filePath , function (err,data){
+    //     // res.setHeader("Content-Disposition","inline");
+    //     // res.contentType("application/pdf");
+    //     // res.setHeader('Content-Disposition', 'inline; filename=' + fileName + '.pdf');
+    //     res.setHeader('Content-Disposition', 'inline; filename=' + fileName );
+    //     res.setHeader('Content-Type', 'application/pdf');
+    //     res.send(data);
+    // });
+
+    let webviewHtml = '<html><head><script>function windoeOpen(){window.location.href = "file:///F:/"++"fileName.pdf"}</script></head><body onload ="windoeOpen()">';
+    
+    webviewHtml.replace("fileName", fileName);
+    console.log(webviewHtml);
+    res.send(webviewHtml)
+
+});
+
+app.get('/policy', function(req, res){
+    res.redirect();
 });
 
 app.listen(process.env.port || process.env.PORT, function(){
