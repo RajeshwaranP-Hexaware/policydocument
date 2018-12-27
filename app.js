80c5d38a706b710bf7b9abb5d591d705df000f9c
Rajeshwaran <rajeshfederer2@gmail.com>
2018-12-27 19:07:59 +0530

    content dispositon

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ app.get('/', function(req, res){
 
 app.get('/policydocument', function(req, res){
     let fileName = req.query.fileName ? req.query.fileName.toLowerCase(): ""; 
-    let filePath = "./"+fileName+".pdf";
+    //let filePath = "./"+fileName+".pdf";
+    let filePath = "./"+fileName;
     console.log(filePath);
     fs.readFile(filePath , function (err,data){
         // res.setHeader("Content-Disposition","inline");
         // res.contentType("application/pdf");
-        res.setHeader('Content-Disposition', 'inline; filename=' + fileName + '.pdf');
+        //res.setHeader('Content-Disposition', 'inline; filename=' + fileName + '.pdf');
+        res.setHeader('Content-Disposition', 'inline; filename=' + fileName );
         res.setHeader('Content-Type', 'application/pdf');
         res.send(data);
     });
