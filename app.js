db82e205e2c7e8c8a6480a80eeaeaf30d2939589
RajeshwaranP-Hexaware <rajeshfederer2@gmail.com>
2018-12-30 13:30:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/policydocument', function(req, res){
     let fileName = req.query.fileName ? req.query.fileName.toLowerCase(): ""; 
     //let filePath = "./"+fileName+".pdf";
     let filePath = "./"+fileName;
-    // console.log(filePath);
+     console.log(filePath);
      fs.readFile(filePath , function (err,data){
          // res.setHeader("Content-Disposition","inline");
          // res.contentType("application/pdf");
