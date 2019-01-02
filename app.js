749e6f5d4c59ac0d98d66a4bb6877ad9d9361983
Rajeshwaran <rajeshfederer2@gmail.com>
2019-01-02 13:06:17 +0530

    trying in iframe

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/policydocument', function(req, res){
 app.get('/docs', function(req, res){
     //let webviewHtml = '<html><head></head><body><iframe src="https://docs.google.com/gview?url=https://policy-test.herokuapp.com/policydocument?fileName=leave%20policy.pdf&embedded=true" onload="javascript:this.contentWindow.location.hash=0.page.20;" style="width: 1000px;height: 1000px;"></body>'
     //let webviewHtml = '<html><head><meta charset="UTF-8"><style>body {margin: 0;}iframe {display: block;background: #000;border: none;height: 1000vh;width: 1000vw;}</iframe></style></head><body><iframe src="https://policy-test.herokuapp.com/policydocument?fileName=leave%20policy.pdf#page=5" style="width: 100px;height: 200px;"></body>';
-    let webviewHtml = '<html><head><meta charset="UTF-8"></head><body><iframe src="https://policy-test.herokuapp.com/policydocument?fileName=leave%20policy.pdf#page=5" </body>';
+    let webviewHtml = '<html><head><meta charset="UTF-8"></head><body><iframe src="https://policy-test.herokuapp.com/policydocument?fileName=leave%20policy.pdf#page=5" width=100% height=100%></iframe></body>';
     res.send(webviewHtml);
 });
 
