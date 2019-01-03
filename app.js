b982da214987b74e962ea96ce259472898366970
Rajeshwaran <rajeshfederer2@gmail.com>
2019-01-03 17:49:22 +0530

    PDF JS

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.get('/docs', function(req, res){
 
 app.get('/pdf', function(req, res){
     console.log(__dirname);
-    res.sendFile(path.join(__dirname + '/public/web/viewer.html?file=leavepolicy.pdf#page=5'));
+    res.sendFile(path.join("" + '/public/web/viewer.html?file=leavepolicy.pdf#page=5'));
 });
 
 app.listen(process.env.port || process.env.PORT, function(){
