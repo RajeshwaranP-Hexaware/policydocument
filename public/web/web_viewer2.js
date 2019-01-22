d23cd834c5d4c6fe6ceba3f9c1d28e8e20091ff1
Rajeshwaran <rajeshfederer2@gmail.com>
2019-01-22 13:14:47 +0530

    adding require js

--- public/web/web_viewer2.js
+++ public/web/web_viewer2.js
@@ -26,7 +26,11 @@ module.exports = {
         'pdfjsDistView': 'https://npmcdn.com/pdfjs-dist/web/pdf_viewer'
     }}};
   </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js"></script>
+  <script data-main = "{requirejs.config({
+    paths: {
+        'pdfjsDistPdf': 'https://npmcdn.com/pdfjs-dist/build/pdf',
+        'pdfjsDistView': 'https://npmcdn.com/pdfjs-dist/web/pdf_viewer'
+    }})}" src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js"></script>
   <script>
         require(['pdfjsDistPdf', 'pdfjsDistView'], function(pdfjsLib, pdfjsViewer) {
             //var pdfjsLib = require('https://npmcdn.com/pdfjs-dist/build/pdf.js');
