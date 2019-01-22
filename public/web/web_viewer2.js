e6f9330b0bd23c9b427bd3279db9aad024041d05
Rajeshwaran <rajeshfederer2@gmail.com>
2019-01-22 14:27:41 +0530

    adding require js

--- public/web/web_viewer2.js
+++ public/web/web_viewer2.js
@@ -19,18 +19,14 @@ module.exports = {
   <script src="https://npmcdn.com/pdfjs-dist/build/pdf.js"></script> 
   <script src="https://npmcdn.com/pdfjs-dist/web/pdf_viewer.js"></script>
   
-  <script>
-  var require = { config : {
-    paths: {
-        'pdfjsDistPdf': 'https://npmcdn.com/pdfjs-dist/build/pdf',
-        'pdfjsDistView': 'https://npmcdn.com/pdfjs-dist/web/pdf_viewer'
-    }}};
-  </script>
-  <script data-main = {requirejs.config({
-    paths: {
-        'pdfjsDistPdf': 'https://npmcdn.com/pdfjs-dist/build/pdf',
-        'pdfjsDistView': 'https://npmcdn.com/pdfjs-dist/web/pdf_viewer'
-    }})}" src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js></script>
+  // <script>
+  // var require = { config : {
+  //   paths: {
+  //       'pdfjsDistPdf': 'https://npmcdn.com/pdfjs-dist/build/pdf',
+  //       'pdfjsDistView': 'https://npmcdn.com/pdfjs-dist/web/pdf_viewer'
+  //   }}};
+  // </script>
+  <script data-main = "/web/config.js" src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js></script>
   <script>
         require(['pdfjsDistPdf', 'pdfjsDistView'], function(pdfjsLib, pdfjsViewer) {
             //var pdfjsLib = require('https://npmcdn.com/pdfjs-dist/build/pdf.js');
