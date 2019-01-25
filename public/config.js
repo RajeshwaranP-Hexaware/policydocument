29706c97a6f2a7b5bbd80dda6f6c4536da36e2c5
Rajeshwaran <rajeshfederer2@gmail.com>
2019-01-25 12:44:47 +0530

    adding require js

---
+++ public/config.js
@@ -0,0 +1,7 @@
+requirejs.config({
+    baseUrl: 'js/',
+    paths: {
+        'pdfjsDistPdf': ['https://npmcdn.com/pdfjs-dist/build/pdf'],
+        'pdfjsDistView': ['https://npmcdn.com/pdfjs-dist/web/pdf_viewer']
+    }}
+)
